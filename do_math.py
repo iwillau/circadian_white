e1c052574a2c9e126ad326e2058ead8d0bba6198
Will Wheatley <will@iwill.id.au>
2020-06-12 01:22:05 +1000

    Plotting the output

--- do_math.py
+++ do_math.py
@@ -2,12 +2,10 @@ import logging
 from homeassistant.util import dt as dt_util
 from datetime import timedelta
 from csv import writer
+import matplotlib.pyplot as plt
 
 
 _LOGGER = logging.getLogger(__name__)
-
-
-dt_util.set_default_time_zone(dt_util.get_time_zone('Australia/Melbourne'))
 
 
 class CircadianWhiteSensor():
@@ -25,12 +23,6 @@ class CircadianWhiteSensor():
         self._last_sun_update = None
         self._state = self._maximum
         self._available = False
-    
-        now = dt_util.now()
-        self._day_start = now.replace(hour=7, minute=1, second=42)
-        self._day_middle = now.replace(hour=12, minute=19, second=32)
-        self._day_end = now.replace(hour=17, minute=37, second=26)
-        self._last_sun_update = now
 
     @property
     def state(self):
@@ -104,30 +96,125 @@ class CircadianWhiteSensor():
 
 
 if __name__ == '__main__':
-    circ = CircadianWhiteSensor(1500, 4500, 6500)
+    # When to simulate
+    dt_util.set_default_time_zone(
+        dt_util.get_time_zone('Australia/Melbourne'))
+    now = dt_util.now()
+    day_start = now.replace(hour=7, minute=1, second=42)
+    day_middle = now.replace(hour=12, minute=19, second=32)
+    day_end = now.replace(hour=17, minute=37, second=26)
+    start = now.replace(hour=0, minute=0, second=0)
 
     def one_day():
-        start = dt_util.now().replace(hour=0, minute=0, second=0)
-        for seconds in range(86400):
-            yield start + timedelta(seconds=seconds)
-
-
+        circ = CircadianWhiteSensor(1500, 4500, 6500)
+        circ._day_start = day_start
+        circ._day_middle = day_middle
+        circ._day_end = day_end
+        circ._last_sun_update = start
+        
+        state = None
+        for seconds in range(86399):
+            now = start + timedelta(seconds=seconds)
+            circ._calculate_kelvins(now)
+            if state != circ.state:
+                state = circ.state
+                yield seconds, now, circ
+        #Yield the final state no matter what
+        now = start + timedelta(seconds=86400)
+        circ._calculate_kelvins(now)
+        yield 86400, now, circ
+        
+    plot_times = []
+    plot_kelvins = []
+    plot_tod = {
+        'times': [],
+        'kelvins': [],
+    }
+    plot_xticks = [0,
+                   (day_start - start).total_seconds(),
+                   (day_middle - start).total_seconds(),
+                   (day_end - start).total_seconds(),
+                   86400]
+    plot_xlabels = ['Midnight', 
+                    'Dawn',
+                    'Noon',
+                    'Dusk', 
+                    'Midnight']
     with open('sample_day.csv', 'w', newline='') as file_h:
         csv = writer(file_h)
         csv.writerow(['Date', 'Time', 'Time of Day', 'Second', 'Kelvins'])
         tod = None
-        state = None
-        for now in one_day():
-            circ._calculate_kelvins(now)
-            if state != circ.state:
-                state = circ.state
-                csv.writerow([now.date(), now.strftime("%H:%M"), 
-                              circ._currently, now.second, circ.state])
-                          
+
+        for seconds, now, circ in one_day():
+            csv.writerow([now.date(), now.strftime("%H:%M"), 
+                          circ._currently, now.second, circ.state])
+            plot_times.append(seconds)
+            plot_kelvins.append(circ.state)
+            
             if tod != circ._currently:
                 tod = circ._currently
-                print("{} {:16} -> {}".format(now.strftime("%H:%M:%S"), tod, circ.state))
+                plot_tod['times'].append(seconds)
+                plot_tod['kelvins'].append(circ.state)
+    
+    with plt.xkcd():
+        fig = plt.figure()
+        
+        ax = fig.add_axes((0.1, 0.1, 0.8, 0.8))
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
+        ax.spines['right'].set_color('none')
+        ax.spines['top'].set_color('none')
+        
+        plt.yticks([1500, 4500, 6500])
+        plt.ylim([1000, 7000])
+        plt.ylabel('Kelvins')
+        
+        ax.set_xlim([0, 86400])
+        ax.set_xticks(plot_xticks)
+        ax.set_xticklabels(plot_xlabels)
+        
+        plt.title("Circadian White level for a sample Day")
+        plt.plot(plot_times, plot_kelvins)
+
+#        from pprint import pprint
+#        pprint(plot_tod)
+#        left = 0
+#        for label, length in plot_tod:
+#            print("Fill: {}  {}->{}".format(label, left, length+1))
+#            plt.fill_between(plot_times[left:length+1],
+#                             1000,
+#                             plot_kelvins[left:length+1], 
+#                             alpha=0.8,
+#            )
+#            left = length+1
+        plt.vlines([4000, 8000], 0, 4000, colors='grey')
+        
+    plt.show()
+    fig.clear()
+    plt.close(fig)
                 
-            
+    
+'''    from pylab import *
+    
+    t = arange(0.0, 2.0, 0.01)
+    s = sin(2*pi*t)
+    plot(t, s)
+    
+    xlabel('time (s)')
+    ylabel('voltage (mV)')
+    title('About as simple as it gets, folks')
+    grid(True)
+    show()
+'''
+
+#	ax.bar([-0.125, 1.0-0.125], [0, 100], 0.25)
+
+
+
+#	ax.set_label('Kelvins')
+#	ax.set_xticks([0, 1])
+#	ax.set_xlim([-0.5, 1.5])
+
+#    ax.	
 
 
