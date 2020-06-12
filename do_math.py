f9fe6afe9921a5e59045cec848015c57e7100b3f
Will Wheatley <will@iwill.id.au>
2020-06-12 17:13:26 +1000

    Re-arrange for HASS

--- do_math.py
+++ do_math.py
@@ -8,94 +8,8 @@ import matplotlib.pyplot as plt
 _LOGGER = logging.getLogger(__name__)
 
 
-class CircadianWhiteSensor():
-
-    def __init__(self, minimum, middle, maximum):
-        """Initialize the Random sensor."""
-        self._minimum = minimum
-        self._middle = middle
-        self._maximum = maximum
-        self._unit_of_measurement = 'kelvins'
-        self._day_start = None
-        self._day_end = None
-        self._day_middle = None
-        self._currently = None
-        self._last_sun_update = None
-        self._state = self._maximum
-        self._available = False
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self._state
-
-
-    def update(self):
-        """Calculate where we are in the current day, the day follows the following sequence:
-              - Night
-              - Early Morning 
-              - Mid Morning
-              - Late Morning
-              - Early Afternoon
-              - Mid Afternoon
-              - Late Afternoon
-              - Evening
-              - Night
-        """
-        if self._last_sun_update is None:
-            self._available = False
-            _LOGGER.info("Circadian White is still waiting for an update from the Sun")
-            return
-
-        now = dt_util.now()
-        # If we haven't been updated for more than 24 hours -> bail
-        if (now - self._last_sun_update).days > 0:
-            self._available = False
-            _LOGGER.warn("Astral data is out of date")
-            return
-
-        self._calculate_kelvins(now)
-        _LOGGER.info("Day is currently: {}".format(self._currently))
-        
-    def _calculate_kelvins(self, now):
-
-        gap = timedelta(hours=2)
-
-        if now < self._day_start:
-            self._currently = 'Night'
-            self._state = self._minimum
-        elif now < (self._day_start + gap):
-            self._currently = 'Early Morning'
-            self._calc_progress(self._minimum, self._middle, gap.seconds, (now - self._day_start).seconds)
-        elif now < (self._day_middle - gap):
-            self._currently = 'Mid Morning'
-            self._state = self._middle
-        elif now < (self._day_middle):
-            self._currently = 'Late Morning'
-            self._calc_progress(self._middle, self._maximum, gap.seconds, (now - self._day_middle + gap).seconds)
-        elif now < (self._day_middle + gap):
-            self._currently = 'Early Afternoon'
-            self._calc_progress(self._maximum, self._middle, gap.seconds, (now - self._day_middle).seconds)
-        elif now < (self._day_end - gap):
-            self._currently = 'Mid Afternoon'
-            self._state = self._middle
-        elif now < (self._day_end):
-            self._currently = 'Late Afternoon'
-            self._calc_progress(self._middle, self._minimum, gap.seconds, (now - self._day_end + gap).seconds)
-        elif now < (self._day_end + gap):
-            self._currently = 'Evening'
-        else:
-            self._currently = 'Night'
-            self._state = self._minimum
-
-    def _calc_progress(self, start, end, length, progress):
-        m_length = end - start  # Actual Length of the Metric (kelvins)
-        percentage_complete = progress / length  # Perc
-        m_progress = percentage_complete * m_length
-        self._state = int(m_progress+start)
-
-
 if __name__ == '__main__':
+    from .sensor import CircadianWhiteSensor
     # When to simulate
     dt_util.set_default_time_zone(
         dt_util.get_time_zone('Australia/Melbourne'))
@@ -111,7 +25,6 @@ if __name__ == '__main__':
         circ._day_middle = day_middle
         circ._day_end = day_end
         circ._last_sun_update = start
-        
         state = None
         for seconds in range(86399):
             now = start + timedelta(seconds=seconds)
@@ -123,7 +36,7 @@ if __name__ == '__main__':
         now = start + timedelta(seconds=86400)
         circ._calculate_kelvins(now)
         yield 86400, now, circ
-        
+
     plot_times = []
     plot_kelvins = []
     plot_tod = {
@@ -135,10 +48,10 @@ if __name__ == '__main__':
                    (day_middle - start).total_seconds(),
                    (day_end - start).total_seconds(),
                    86400]
-    plot_xlabels = ['Midnight', 
+    plot_xlabels = ['Midnight',
                     'Dawn',
                     'Noon',
-                    'Dusk', 
+                    'Dusk',
                     'Midnight']
     with open('sample_day.csv', 'w', newline='') as file_h:
         csv = writer(file_h)
@@ -146,42 +59,42 @@ if __name__ == '__main__':
         tod = None
 
         for seconds, now, circ in one_day():
-            csv.writerow([now.date(), now.strftime("%H:%M"), 
+            csv.writerow([now.date(), now.strftime("%H:%M"),
                           circ._currently, now.second, circ.state])
             plot_times.append(seconds)
             plot_kelvins.append(circ.state)
-            
+
             if tod != circ._currently:
                 tod = circ._currently
                 plot_tod['times'].append(seconds)
                 plot_tod['kelvins'].append(circ.state)
                 print("{} {:16} -> {}".format(
                     now.strftime("%H:%M:%S"), tod, circ.state))
-    
+
     with plt.xkcd():
         fig = plt.figure()
-        
+
         ax = fig.add_axes((0.1, 0.1, 0.8, 0.8))
         ax.xaxis.set_ticks_position('bottom')
         ax.yaxis.set_ticks_position('left')
         ax.spines['right'].set_color('none')
         ax.spines['top'].set_color('none')
-        
+
         plt.yticks([1500, 4500, 6500])
         plt.ylim([1000, 7000])
         plt.ylabel('Kelvins')
-        
+
         ax.set_xlim([0, 86400])
         ax.set_xticks(plot_xticks)
         ax.set_xticklabels(plot_xlabels)
-        
+
         plt.title("Circadian White level for a sample Day")
         plt.plot(plot_times, plot_kelvins)
 
         plt.vlines(plot_tod['times'], 0, plot_tod['kelvins'], colors='grey')
-        
+
     plt.show()
     fig.clear()
     plt.close(fig)
-                
+
 
